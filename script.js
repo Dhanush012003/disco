16de32852be08ad02b80ec64b222f150b0009312
Dhanush Kumar S S <111297810+Dhanush012003@users.noreply.github.com>
2023-07-25 10:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function set(){
+    setTimeout((()=> {
+        document.body.style.background = "linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,1)),url('https://gumlet.assettype.com/filmcompanion%2F2023-07%2F48e952db-b61b-405e-b2c6-248b3dea6b66%2FScreenshot_2023_07_17_at_6_21_18_PM.png?format=auto')"
+        document.body.style.backgroundSize = "cover"
+        document.getElementById("h1").innerHTML= "UN PERA THUKKA NAALU PERU";
+    }), 400);
+    setTimeout((()=> {
+        document.body.style.background = "linear-gradient(rgba(255,29,29,.2), rgba(231,80,236,0.479)),url('https://wallpapercave.com/wp/wp9801009.jpg')"
+        document.body.style.backgroundSize = "cover"
+        document.getElementById("h1").innerHTML= "PATATHA PARIKA NOORU PERU";
+    }),800);
+    setTimeout((()=> {
+        document.body.style.background = "linear-gradient(rgba(2,255,213,.2), rgba(255,23,23)),url('https://wallpaperaccess.com/full/2761343.jpg')"
+        document.body.style.backgroundSize = "cover"
+        document.getElementById("h1").innerHTML= "KUTTI CHEVTHA ETTI PARTHA";
+    }),1200);
+    setTimeout((()=> {
+        document.body.style.background = "linear-gradient(rgba(255, 0, 0, 0.2), rgba(3, 255, 45)),url('http://2.bp.blogspot.com/-wQtZSiSOMJ0/UzbvyyReavI/AAAAAAAAUdY/J6X_skjBMDk/s1600/rajnikanth_072.jpg')"
+        document.body.style.backgroundSize = "cover"
+        document.getElementById("h1").innerHTML= "USURA KUDUKA KOODI PERU";
+    }),1400);
+}
+setInterval(set,1500)
